bba3e79b2e125f8b385a5e999063fd1ba61de478
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-01-22 21:26:39 +0200

    truncate string

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
+
 require([
 	'dojo/dom',
 	'demo/myModule',
+  'dojox/html/ellipsis',
 	'dojo/domReady!'
 ], function (dom, myModule) {
-	myModule.setText('greeting', 'Hello Dojo!');
+  const txt = dom.byId('cutMe');
+  if (txt !== ""){
+    let cut = txt.innerHTML;
+    if (cut.length > 20) {
+      cut=cut.substring(0, 25); // don't cut words
+      cut=cut.replace(/\w+$/, '');
+      cut += '...';
+      txt.innerHTML = cut;
+    }
+  }
 
+	myModule.setText('greeting', 'Hello Dojo!');
 	setTimeout(function () {
 		myModule.restoreText('greeting');
 	}, 3000);
+
+
+
 });
